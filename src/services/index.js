1f9aed31e5b2416a70d3bb5796e0b272ef4eb4dc
Akshay Chauhan <chauhanakshay395@gmail.com>
2025-03-06 20:09:22 +0530

    Added common crud classes for basic crud operations

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,6 @@
 module.exports = {
     CityService: require("../services/city-service"),
-    FlightService: require("../services/flight-service")
+    FlightService: require("../services/flight-service"),
+    AirportService: require("./airport-service"),
+    CrudService: require("./crud-service")
 }
